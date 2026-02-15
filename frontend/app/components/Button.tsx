e8a10fb87ef1bb8dc46f82e65f779fbe39c1a258
Elkwizard <bananamanthefourth@gmail.com>
2026-02-14 19:23:33 -0800

    improved Button padding

--- frontend/app/components/Button.tsx
+++ frontend/app/components/Button.tsx
@@ -17,9 +17,9 @@ export default function Button({
   const styles =
     {
       primary:
-        'p-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition text-center',
+        'py-2 px-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition text-center',
       secondary:
-        'p-2 text-gray-600 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg text-center',
+        'py-2 px-4 text-gray-600 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg text-center',
     }[style] + (tw ? ` ${tw}` : '');
 
   if (href) {
