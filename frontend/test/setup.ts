673463f4d11b270e989887c5b28366bfec9b0039
Elkwizard <bananamanthefourth@gmail.com>
2026-02-14 20:32:17 -0800

    added <dialog> mocking to setup.ts

--- frontend/test/setup.ts
+++ frontend/test/setup.ts
@@ -1,7 +1,20 @@
 import '@testing-library/jest-dom/vitest';
 import {cleanup} from '@testing-library/react';
-import {afterEach} from 'vitest';
+import {afterEach, beforeAll, vi} from 'vitest';
 import './mocks/auth0';
+
+beforeAll(() => {
+  HTMLDialogElement.prototype.showModal = vi.fn(function mock(
+    this: HTMLDialogElement,
+  ) {
+    this.open = true;
+  });
+  HTMLDialogElement.prototype.close = vi.fn(function mock(
+    this: HTMLDialogElement,
+  ) {
+    this.open = false;
+  });
+});
 
 // Automatically clean up the DOM after each test
 afterEach(() => {
