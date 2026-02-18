db82881ee6e039f3505df32161e61b6a89aaaba2
Elkwizard <bananamanthefourth@gmail.com>
2026-02-17 23:28:01 -0800

    commented out drivers and added better scripts

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ BACKEND_BUILD="./backend/src/main/resources/static"
 cd frontend
 npm install
 npm run build
+cd ..
 rm -rf "$BACKEND_BUILD"
 mkdir "$BACKEND_BUILD"
 cp -r "$FRONTEND_BUILD"/* "$BACKEND_BUILD"
-# npm run db:up
---
+++ run.sh
@@ -0,0 +1,3 @@
+npm run db:up
+cd backend
+./mvnw spring-boot:run
