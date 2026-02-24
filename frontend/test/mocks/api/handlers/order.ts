d9d8846b23312315315a75b80d3254416249a2f4
Elkwizard <bananamanthefourth@gmail.com>
2026-02-23 17:58:17 -0800

    fixed remake handling in order API mock

--- frontend/test/mocks/api/handlers/order.ts
+++ frontend/test/mocks/api/handlers/order.ts
@@ -43,18 +43,19 @@ export const orderHandlers = [
     if (!order) return notFound('order');
 
     const domain = json.order.parse(order);
+    const now = Date.now();
     db.orders.update(
       json.order.unparse({
         ...domain,
         state: 'cooking',
-        initialTime: new Date(),
+        initialTime: new Date(now),
         currentBatch: null,
         highPriority: true,
         cookedTime: new Date(
-          domain.cookedTime.getTime() - domain.initialTime.getTime(),
+          now + domain.cookedTime.getTime() - domain.initialTime.getTime(),
         ),
         deliveryTime: new Date(
-          domain.deliveryTime.getTime() - domain.initialTime.getTime(),
+          now + domain.deliveryTime.getTime() - domain.initialTime.getTime(),
         ),
       }),
     );
