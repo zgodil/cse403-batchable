c4a0bcbde58bcf1c9668d6f4337bdca2fced890e
Elkwizard <bananamanthefourth@gmail.com>
2026-02-24 23:22:42 -0800

    improved useLoader() mock to be able to fake specific behavior when needed. this may be improved in the future

--- frontend/test/mocks/query.ts
+++ frontend/test/mocks/query.ts
@@ -1,13 +1,42 @@
+import {act} from '@testing-library/react';
+import {beforeEach} from 'node:test';
+import {useEffect, useState} from 'react';
 import {vi} from 'vitest';
+import {type Loader} from '~/util/query';
 
+const LOADING_STATE = {
+  response: null,
+  loaded: false,
+  reload: () => {},
+} as const;
+
+// The handleLoader property can be set to specify a loading result associated with a given async loader function
+export const loaderMock: {
+  handleLoader: <T>(load: () => Promise<T>) => Promise<Partial<Loader<T>>>;
+} = {
+  handleLoader: async () => ({}),
+};
+
+// pretends to be useLoader in such a way that individual tests can mock the return value
 vi.mock('~/util/query.ts', () => {
   return {
-    useLoader: () => {
-      return {
-        response: [],
-        loaded: true,
-        reload: () => {},
-      };
+    useLoader<T>(load: () => Promise<T>) {
+      const [result, setResult] = useState<Loader<T>>(LOADING_STATE);
+      useEffect(() => {
+        void loaderMock.handleLoader(load).then(state => {
+          return act(() => {
+            setResult({
+              ...LOADING_STATE,
+              ...state,
+            });
+          });
+        });
+      }, []);
+      return result;
     },
   };
+});
+
+beforeEach(() => {
+  loaderMock.handleLoader = async () => ({});
 });
