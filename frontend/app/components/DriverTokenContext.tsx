985a2b10e0807cccf241865a6268315094cdc3cc
Elkwizard <bananamanthefourth@gmail.com>
2026-03-10 19:45:50 -0700

    documented DriverTokenContext

--- frontend/app/components/DriverTokenContext.tsx
+++ frontend/app/components/DriverTokenContext.tsx
@@ -1,3 +1,4 @@
 import {createContext} from 'react';
 
+/** represents the hidden driver token used to identify the /route/:token page  */
 export const DriverTokenContext = createContext<string | null>(null);
